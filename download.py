3f630c15e8c69e4c677f7aee739d9337b217f5bc
kandation <akkhaporn@gmail.com>
2017-05-25 14:04:06 +0700

    init & finish

---
+++ download.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+url_id = 11
+url = "https://www3.reg.cmu.ac.th/regist359/public/result.php?id=5806106"
+counter = 0
+while url_id < 99:
+    times = time.time()
+    print("Snapshot Content 5806106"+str(url_id).zfill(3)+ " tarting")
+    service_args = [
+        '--ignore-ssl-errors=true',
+        '--cookies-file=test.cookies',
+        '--disk-cache=true'
+        ]
+    driver = webdriver.PhantomJS(service_args=service_args)
+    driver.set_window_size(1366, 768) # set the window size that you need
+    driver.get(url+str(url_id))
+    driver.save_screenshot('2502/12t_'+str(url_id).zfill(3)+'.png')
+    print("\tSnapshot Content 5806106"+str(url_id)+" Finished! @"+str(round(time.time()-times,3)))
+    url_id += 1
